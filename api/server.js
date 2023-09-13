f7dd108afafe2a8e2c7e771ed63aa819723ebe6c
Victoria Blancas <vikki6688@gmail.com>
2023-09-13 15:47:25 -0400

    endpoints wired and working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 
+//add accounts
+const accountsRouter = require('./accounts/accounts-router')
+
 const server = express();
 
 server.use(express.json());
+
+//connect accountsRouter
+server.use('/api/accounts', accountsRouter)
+
+//fallback code
+server.use('*', (req, res)=>{
+    res.status(404).json({
+        message:'not found',
+    })
+})
 
 module.exports = server;
